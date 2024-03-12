f8fb8426a6eb7cc952c57efd17b94649af33c81d
Sayid <sayid@gmail.com>
2024-03-12 18:21:58 +0500

    Adding view by using pug(first developments)

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -1,6 +1,18 @@
 const home_controller = {
     index: async (req, res) =>{
         res.render('home');
+    },
+    add: async (req, res) =>{
+        res.render('home/add_update')
+    },
+    update: async (req, res) =>{
+        res.render('home/add_update')
+    },
+    about: async (req, res) =>{
+        res.render('home/about')
+    },
+    contacts: async (req, res) =>{
+        res.render('home/contacts')
     }  
 };
   
--- routes/web/home/index.js
+++ routes/web/home/index.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const home_controller = require('../../../controllers/web/home');
 
 router.get('/', home_controller.index);
+router.get('/add', home_controller.add);
+router.get('/update', home_controller.update);
+router.get('/about', home_controller.about);
+router.get('/contacts', home_controller.contacts);
+
 
 module.exports = router;
